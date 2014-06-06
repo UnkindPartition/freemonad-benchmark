27ff6032479ec0fb46204e67e0680a94b4fa853f
Roman Cheplyaka <roma@ro-che.info>
2014-06-06 18:11:21 +0200

    mtl

--- Bench.hs
+++ Bench.hs
@@ -7,6 +7,7 @@ import qualified Church
 import qualified Codensity
 import qualified NoRemorse
 import Control.Monad
+import qualified Control.Monad.State.Strict as MTL
 
 import Criterion (bench, nf)
 import Criterion.Main (defaultMain)
@@ -19,6 +20,11 @@ computation n = forM_ [1..n] $ \_ -> do
   s <- get
   put $! s + 1
 
+mtlComputation :: Int -> MTL.State Int ()
+mtlComputation n = forM_ [1..n] $ \_ -> do
+  s <- MTL.get
+  MTL.put $! s + 1
+
 n = 20
 
 main :: IO ()
@@ -28,4 +34,5 @@ main = defaultMain
   , bench "Chruch" $ nf (flip Church.run 0 . computation) n
   , bench "Codensity" $ nf (flip Codensity.run 0 . computation) n
   , bench "NoRemorse" $ nf (flip NoRemorse.run 0 . computation) n
+  , bench "MTL" $ nf (flip MTL.runState 0 . mtlComputation) n
   ]
