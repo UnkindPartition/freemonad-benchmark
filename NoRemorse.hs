7cfdc6326768436b34bef440c165c3f0533bc359
Roman Cheplyaka <roma@ro-che.info>
2015-12-10 12:13:46 +0200

    NoRemorse: update for AMP, fix warnings

--- NoRemorse.hs
+++ NoRemorse.hs
@@ -6,6 +6,7 @@ module NoRemorse where
 
 import Data.Interface.TSequence
 import Data.FastTCQueue
+import Control.Monad
 import Base
 type TCQueue = FastTCQueue
 
@@ -13,8 +14,8 @@ newtype FC f a b = FC (a -> FreeMonad f b)
 type FMExp f a b = TCQueue (FC f) a b
 data FreeMonad f a = 
    forall x. FM (FreeMonadView f x) (FMExp f x a)
-data FreeMonadView f a 	= Pure a 
-                        | Impure (f (FreeMonad f a))
+data FreeMonadView f a = Pure a 
+                       | Impure (f (FreeMonad f a))
 fromView x = FM x tempty
 
 toView :: Functor f => FreeMonad f a -> FreeMonadView f a
@@ -27,6 +28,11 @@ toView (FM h t) = case h of
  where (>>>=) :: FreeMonad f a -> FMExp f a b -> FreeMonad f b 
        (FM h t) >>>= r = FM h (t >< r)
 
+instance Functor (FreeMonad f) where
+  fmap = liftM
+instance Applicative (FreeMonad f) where
+  pure = return
+  (<*>) = ap
 instance Monad (FreeMonad f) where
   return = fromView . Pure
   (FM m r) >>= f = FM m (r >< tsingleton (FC f))
