39a888417ab18af3691e269d4725ca00815acfc0
Benjamin Franksen <Ben Franksen benjamin.franksen@helmholtz-berlin.de>
2016-11-08 20:41:38 +0100

    Add Fused
    To demonstrate the efficiency of the new implementation (almost (half) as
    good as  as MTL), I needed to add (type-) specialized versions of the test
    computations. To further demonstrate the impact of specialization, also
    added (type-) generalized versions for MTL, resulting in huge efficiency
    loss.

--- Main.hs
+++ Main.hs
@@ -8,32 +8,72 @@ import qualified Church
 import qualified Codensity
 import qualified NoRemorse
 import qualified Freer
+import qualified Fused
 import Control.Monad
 import qualified Control.Monad.State.Strict as MTL
 
 import Criterion (bench, nf, bgroup, Benchmark)
 import Criterion.Main (defaultMain)
 
-
 n = 50
 
-benchmarks
-  :: (forall m . (Monad m, MonadFree F m) => Int -> m ())
-  -> (Int -> MTL.State Int ())
-  -> Int
-  -> [Benchmark]
-benchmarks computation mtlComputation n =
-  [ bench "Free" $ nf (flip Free.run 0 . computation) n
-  , bench "Free/lazy" $ nf (flip Free.runLazily 0 . computation) n
-  , bench "Chruch" $ nf (flip Church.run 0 . computation) n
-  , bench "Codensity" $ nf (flip Codensity.run 0 . computation) n
-  , bench "NoRemorse" $ nf (flip NoRemorse.run 0 . computation) n
-  , bench "Freer" $ nf (flip Freer.run 0 . computation) n
-  , bench "MTL" $ nf (flip MTL.runState 0 . mtlComputation) n
+{-
+-- Passing computations with higher rank 'forall m...'
+-- types destroys specialization!
+-- This makes Fused/monadstate as slow as Fused/generic.
+
+type Comp m = Int -> m ()
+
+benchmarks :: (forall m. MonadFree F m => Comp m)
+           -> (forall m. MTL.MonadState Int m => Comp m)
+           -> Comp (Fused.Codensity Fused.H)
+           -> Comp (MTL.State Int)
+           -> [Benchmark]
+benchmarks generic monadstate fused mtl = 
+  [ bench "Free strict/generic" $ run_bench (Free.run . generic) n
+  , bench "Free lazy/generic" $ run_bench (Free.runLazily . generic) n
+  , bench "Church/generic" $ run_bench (Church.run . generic) n
+  , bench "Codensity/generic" $ run_bench (Codensity.run . generic) n
+  , bench "NoRemorse/generic" $ run_bench (NoRemorse.run . generic) n
+  , bench "Freer/generic" $ run_bench (Freer.run . generic) n
+  , bench "Fused/generic" $ run_bench (Fused.run . generic) n
+  , bench "Fused/monadstate" $ run_bench (Fused.run . monadstate) n
+  , bench "Fused/specialized" $ run_bench (Fused.run . fused) n
+  , bench "MTL/generic" $ run_bench (MTL.runState . monadstate) n
+  , bench "MTL/specialized" $ run_bench (MTL.runState . mtl) n
   ]
+-}
+
+run_bench comp = nf $ \n -> comp n 0
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "Right-assoc" $ benchmarks computation mtlComputation n
-  , bgroup "Left-assoc" $ benchmarks computation2 mtlComputation2 n
+  [ bgroup "Right-assoc" $
+    -- benchmarks computation msComputation fusedComputation mtlComputation
+    [ bench "Free strict/generic" $ run_bench (Free.run . computation) n
+    , bench "Free lazy/generic" $ run_bench (Free.runLazily . computation) n
+    , bench "Church/generic" $ run_bench (Church.run . computation) n
+    , bench "Codensity/generic" $ run_bench (Codensity.run . computation) n
+    , bench "NoRemorse/generic" $ run_bench (NoRemorse.run . computation) n
+    , bench "Freer/generic" $ run_bench (Freer.run . computation) n
+    , bench "Fused/generic" $ run_bench (Fused.run . computation) n
+    , bench "Fused/monadstate" $ run_bench (Fused.run . msComputation) n
+    , bench "Fused/specialized" $ run_bench (Fused.run . fusedComputation) n
+    , bench "MTL/generic" $ run_bench (MTL.runState . msComputation) n
+    , bench "MTL/specialized" $ run_bench (MTL.runState . mtlComputation) n
+    ]
+  , bgroup "Left-assoc" $
+    -- benchmarks computation2 msComputation2 fusedComputation2 mtlComputation2
+    [ bench "Free strict/generic" $ run_bench (Free.run . computation2) n
+    , bench "Free lazy/generic" $ run_bench (Free.runLazily . computation2) n
+    , bench "Church/generic" $ run_bench (Church.run . computation2) n
+    , bench "Codensity/generic" $ run_bench (Codensity.run . computation2) n
+    , bench "NoRemorse/generic" $ run_bench (NoRemorse.run . computation2) n
+    , bench "Freer/generic" $ run_bench (Freer.run . computation2) n
+    , bench "Fused/generic" $ run_bench (Fused.run . computation2) n
+    , bench "Fused/monadstate" $ run_bench (Fused.run . msComputation2) n
+    , bench "Fused/specialized" $ run_bench (Fused.run . fusedComputation2) n
+    , bench "MTL/generic" $ run_bench (MTL.runState . msComputation2) n
+    , bench "MTL/specialized" $ run_bench (MTL.runState . mtlComputation2) n
+    ]
   ]
